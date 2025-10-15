64e798f8abf7887891556e1724439d35bf3916a9
MahefaNant <mahefanant@gmail.com>
2025-10-15 13:49:04 +0300

    fix(auth-service): fix signup in auth service

--- econolink-backend/src/auth/auth.service.ts
+++ econolink-backend/src/auth/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
     });
 
     const payload = { sub: user.id, email: user.email, name: user.name };
-    const token = this.jwtService.signAsync(payload);
+    const token = await this.jwtService.signAsync(payload);
 
     return {
       user: { id: user.id, email: user.email, name: user.name },
