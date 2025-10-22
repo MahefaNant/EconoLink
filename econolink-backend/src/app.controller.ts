b4bb05cdf70aa9881012119ad162fdb772d47257
MahefaNant <mahefanant@gmail.com>
2025-10-22 16:34:42 +0300

    feat(health-check): add health check request from back end to check if the connection is showing up

--- econolink-backend/src/app.controller.ts
+++ econolink-backend/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Header } from "@nestjs/common";
 import { AppService } from "./app.service";
 
-@Controller()
+@Controller("app")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("health-check")
+  @Header("Cache-Control", "no-cache, no-store, must-revalidate")
+  healthCheck() {
+    return {
+      status: "ok",
+      timestamp: new Date().toISOString(),
+      service: "econolink-back-end",
+    };
   }
 }
